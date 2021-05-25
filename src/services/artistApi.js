f3b50e2471f43ac51a2a2580d8541ab5cb153c6a
Dominique Sakrisson <domsakrisson@gmailcom>
2021-05-25 16:14:24 -0700

    Write api functions

---
+++ src/services/artistApi.js
@@ -0,0 +1,46 @@
+export async function getArtists(query,offset){
+    const res = await fetch(`http://musicbrainz.org/ws/2/artist?query=${query}&fmt=json&limit=5&offset=${offset}`)
+    const {artists} = await res.json();
+
+  return artists.map((artist) => ({
+      id: artist.id, 
+      type: artist.type,
+      name: artist.name,
+      distinction: artist.disambiguation || 'Original Artist here',
+  }))
+}
+
+export async function getReleases(artistId, artistName) {
+    const res = await fetch(`http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`);
+
+    const {releases} = await res.json();
+    
+    return releases.map((release) => ({
+        id: release.id,
+        artist: artistName,
+        title: release.title,
+        image: `http://coverartarchive.org/release/${release.id}front` || 'http://placekitten.com/300/300'
+    }))
+}
+
+export async function getSongs(releaseId, artistName){
+    const res = fetch(`http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`);
+
+    const {recordings} = await res.json();
+    // const songRes = fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    // const {lyrics} = await songRes.json();
+
+    return recordings.map((song) => ({
+        id: song.id,
+        artist: artistName,
+        title: song.title,
+        lyrics: lyrics,
+    }))
+}
+
+export async function getLyrics(artist, title){
+    const res = fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    const {lyrics} = await res.json();
+    return lyrics;
+}
+
